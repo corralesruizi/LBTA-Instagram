d791368b942d4d734eac2debdff6124f6be7f34d
Isaac Corrales Ruiz <icorrales@itexico.net>
2019-05-14 09:44:50 -0500

    adds photo selection capabilities

--- InstagramFirebase/InstagramFirebase/ViewControllers/AddPhotos/AddPhotosViewController.swift
+++ InstagramFirebase/InstagramFirebase/ViewControllers/AddPhotos/AddPhotosViewController.swift
@@ -5,6 +5,8 @@ class AddPhotosViewController: UIViewController {
 
     @IBOutlet weak var cvPhotos: UICollectionView!
     var images = [UIImage]()
+    var phAssets = [PHAsset]()
+    var selectedImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,42 +28,51 @@ class AddPhotosViewController: UIViewController {
     
     fileprivate func setupNavigationButtons() {
         navigationController?.navigationBar.tintColor = .black
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .done, target: self, action: #selector(handleCancel))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(handleNext))
     }
     
-    
-    fileprivate func fetchPhotosFromLibrary()
-    {
+    fileprivate func assetsFetchOptions() -> PHFetchOptions{
         let fetchOptions = PHFetchOptions()
         let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
         
         fetchOptions.fetchLimit = 10
         fetchOptions.sortDescriptors = [sortDescriptor]
+        return fetchOptions
+    }
+    
+    fileprivate func fetchPhotosFromLibrary()
+    {
+        let allPhotos = PHAsset.fetchAssets(with: .image, options: assetsFetchOptions())
         
-        let allPhotos = PHAsset.fetchAssets(with: .image, options: fetchOptions)
-        
-        allPhotos.enumerateObjects({ (asset, count, stop) in
-            let imageManager = PHImageManager.default()
-            let targetSize = CGSize(width: 350, height: 350)
-            let options = PHImageRequestOptions()
-            options.isSynchronous = true
-            imageManager.requestImage(for: asset, targetSize: targetSize,contentMode: .aspectFit, options: options,
-                                      resultHandler: { [weak self](image, info) in
-                                        
-                                        if let image = image {
-                                            self?.images.append(image)
-                                        }
-                                        
-                                        
-                                        DispatchQueue.main.async {
-                                            if count == allPhotos.count - 1 {
-                                                self?.cvPhotos?.reloadData()
-                                            }
-                                        }
-            })
+        DispatchQueue.global(qos: .background).async {
             
-        })
+            allPhotos.enumerateObjects({ (asset, count, stop) in
+                let imageManager = PHImageManager.default()
+                let targetSize = CGSize(width: 200, height: 200)
+                let options = PHImageRequestOptions()
+                options.isSynchronous = true
+                imageManager.requestImage(for: asset, targetSize: targetSize,contentMode: .aspectFit, options: options,
+                                          resultHandler: { [weak self](image, info) in
+                                            
+                                            if let image = image {
+                                                self?.images.append(image)
+                                                self?.phAssets.append(asset)
+                                                
+                                                if self?.selectedImage == nil {
+                                                    self?.selectedImage=image
+                                                }
+                                            }
+                                            
+                                            if count == allPhotos.count - 1 {
+                                                DispatchQueue.main.async {
+                                                    self?.cvPhotos?.reloadData()
+                                                }
+                                            }
+                })
+                
+            })
+        }
         
     }
     
@@ -82,6 +93,11 @@ extension AddPhotosViewController:UICollectionViewDataSource,UICollectionViewDel
         return images.count;
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedImage = images[indexPath.item]
+        self.cvPhotos.reloadData()
+    }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath)
         -> UICollectionViewCell {
             let imagecell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.cellKey,
@@ -95,6 +111,24 @@ extension AddPhotosViewController:UICollectionViewDataSource,UICollectionViewDel
                         at indexPath: IndexPath) -> UICollectionReusableView {
         let header = collectionView.dequeueReusableSupplementaryView(
             ofKind: kind, withReuseIdentifier: AddPhotosHeaderView.cellKey, for: indexPath) as! AddPhotosHeaderView
+        
+        header.imgPhoto.image = selectedImage
+        
+        if let selectedImage = selectedImage {
+            if let index = self.images.firstIndex(of: selectedImage) {
+                let selectedAsset = self.phAssets[index]
+                
+                let imageManager = PHImageManager.default()
+                let targetSize = CGSize(width: 600, height: 600)
+                imageManager.requestImage(for: selectedAsset, targetSize: targetSize, contentMode: .default, options: nil, resultHandler: { (image, info) in
+                    
+                    header.imgPhoto.image = image
+                })
+                
+            }
+        }
+
+        
         
         return header
     }
