e603a7a867e02691d94d854c0fe525c55119c74c
Isaac Corrales Ruiz <icorrales@itexico.net>
2019-05-20 14:23:58 -0500

    fixes follow and unfollowing feature

--- InstagramFirebase/InstagramFirebase/ViewControllers/UserProfile/Cells/HeaderCollectionViewCell.swift
+++ InstagramFirebase/InstagramFirebase/ViewControllers/UserProfile/Cells/HeaderCollectionViewCell.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 class HeaderCollectionViewCell: UICollectionViewCell {
 
@@ -11,10 +12,12 @@ class HeaderCollectionViewCell: UICollectionViewCell {
     
     var user: User?{
         didSet{
-            SetupUI()
+            
             guard let profileImageUrl = user?.profileImageUrl else { return }
             imgProfile.loadImage(urlString: profileImageUrl)
             lblUserName.text = user?.username
+            SetupUI()
+            setupEditFollowButton()
         }
     }
     
@@ -50,7 +53,6 @@ class HeaderCollectionViewCell: UICollectionViewCell {
         btnEdit.backgroundColor = .mainAppColor
         btnEdit.setTitleColor(.white, for: .normal)
         btnEdit.layer.borderColor = UIColor(white: 0, alpha: 0.2).cgColor
-        btnEdit.layer.borderColor = UIColor(white: 0, alpha: 0.2).cgColor
     }
     
     fileprivate func setupUnfollowStyle()
@@ -60,4 +62,65 @@ class HeaderCollectionViewCell: UICollectionViewCell {
         btnEdit.setTitleColor(.black, for: .normal)
         btnEdit.layer.borderColor = UIColor(white: 0, alpha: 0.2).cgColor
     }
+    
+    
+    fileprivate func setupEditFollowButton() {
+        guard let currentLoggedInUserId = Auth.auth().currentUser?.uid else { return }
+        guard let userId = user?.uid else { return }
+        
+        if currentLoggedInUserId == userId {
+            //edit profile
+        } else {
+            Database.database().reference().child("following").child(currentLoggedInUserId).child(userId)
+                .observeSingleEvent(of: .value, with: { [weak self](snapshot) in
+                
+                if let isFollowing = snapshot.value as? Int, isFollowing == 1 {
+                    
+                    self?.btnEdit.setTitle("Unfollow", for: .normal)
+                    
+                } else {
+                    self?.setupFollowStyle()
+                }
+                
+            }, withCancel: { (err) in
+                print("Failed to check if following:", err)
+            })
+        }
+    }
+    
+    @IBAction func EditAction(_ sender: UIButton) {
+        
+        print("Execute edit profile / follow / unfollow logic...")
+        guard let currentLoggedInUserId = Auth.auth().currentUser?.uid else { return }
+        guard let userId = user?.uid else { return }
+        
+        if btnEdit.titleLabel?.text == "Unfollow" {
+            //unfollowing
+            Database.database().reference().child("following").child(currentLoggedInUserId).child(userId)
+                .removeValue(completionBlock: { [weak self](err, ref) in
+                    
+                if let err = err {
+                    print("Failed to unfollow user:", err)
+                    return
+                }
+                
+                print("Successfully unfollowed user:", self?.user?.username ?? "")
+                self?.setupFollowStyle()
+            })
+        }else {
+            let ref = Database.database().reference().child("following").child(currentLoggedInUserId)
+            let values = [userId: 1]
+            
+            ref.updateChildValues(values) { [weak self] (err, ref) in
+                if let err = err {
+                    print("Failed to follow user:", err)
+                    return
+                }
+                print("Successfully followed user: ", self?.user?.username ?? "")
+                self?.setupUnfollowStyle()
+            }
+        }
+        
+    }
+    
 }
