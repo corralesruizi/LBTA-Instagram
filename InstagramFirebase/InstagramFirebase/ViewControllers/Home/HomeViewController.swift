e26c351d72cc45b2cd5b3c5b58df44f8cf102d45
Isaac Corrales Ruiz <icorrales@itexico.net>
2019-05-31 09:29:12 -0500

    refactors home view controller

--- InstagramFirebase/InstagramFirebase/ViewControllers/Home/HomeViewController.swift
+++ InstagramFirebase/InstagramFirebase/ViewControllers/Home/HomeViewController.swift
@@ -71,27 +71,23 @@ class HomeViewController: UIViewController,UIScrollViewDelegate,HomePostCellDele
 extension HomeViewController:UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return posts.count;
+        
+        if let posts = homeVM.posts.value {
+            return posts.count
+        }
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath)
         -> UICollectionViewCell {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! HomeFeedCollectionViewCell
-            cell.post = posts[indexPath.item]
+            
+            if let posts = homeVM.posts.value{
+                cell.post = posts[indexPath.item]
+            }
+            
             cell.delegate=self
             return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
